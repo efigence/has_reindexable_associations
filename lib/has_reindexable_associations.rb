d362df468f3c9e139c7a5b303bd8f7c197dd4f95
Marcin Kalita <rubyconvict@gmail.com>
2016-02-22 13:06:18 +0100

    Added concern with tests

--- lib/has_reindexable_associations.rb
+++ lib/has_reindexable_associations.rb
@@ -1,5 +1,113 @@
 require "has_reindexable_associations/version"
+require 'rails/all'
+require 'searchkick'
 
+# To avoid full model reindex, one can't just call reindex on a scope.
+# One must call reindex_async on each associated record.
+# Usage:
+# MyClass < ActiveRecord::Base
+# include HasReindexableAssociations
+# belongs_to :some_association
+# has_many :some_associations
+# has_reindexable_associations :some_association, :some_associations
 module HasReindexableAssociations
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  class_methods do
+    cattr_writer :reindexable_associations
+
+    # Set this to `true` before any seeds or imports to reindex associations later on your own like this:
+    # MyClass.reindexable_associations_skip = true
+    # MyClass.import(...)
+    # MyClass.reindex
+    # MyClass.reindexable_associations_skip = false
+    # MyClass.reindexable_associations.each { |association| MyClass.send(association).model.reindex }
+    cattr_accessor :reindexable_associations_skip
+
+    def has_reindexable_associations(*args)
+      # @@ is shared between classes via concern, not what we want
+      # @@reindexable_associations = args
+      self.class_variable_set :@@reindexable_associations, args
+    end
+
+    def reindexable_associations
+      # @@reindexable_associations || []
+      self.class_variable_get :@@reindexable_associations
+    rescue NameError
+      []
+    end
+  end
+
+  included do
+    after_commit :reindex_associations
+
+    private
+
+    def reindex_associations
+      self.class.reindexable_associations.each do |reindexable_association|
+        reindex_associations_process(reindexable_association)
+      end
+    end
+
+    def reindex_associations_process(reindexable_association)
+      record_or_records = reindex_associations_scope(reindexable_association)
+      if record_or_records.respond_to?(:to_a)
+        reindex_associations_process_records(record_or_records, reindexable_association)
+      else
+        reindex_associations_process_record(record_or_records, reindexable_association)
+      end
+    end
+
+    def reindex_associations_scope(reindexable_association)
+      begin
+        record_or_records = self.send(reindexable_association)
+      rescue NoMethodError
+        fail NoMethodError.new("foreign key '#{reindexable_association}' is not defined, verify has_reindexable_associations options")
+      end
+    end
+
+    def reindex_associations_process_records(records, reindexable_association)
+      unless self.class.reindexable_associations_skip
+        records.to_a.each do |record|
+          reindex_associations_process_record(record, reindexable_association)
+        end
+      end
+    end
+
+    def reindex_associations_process_record(record, reindexable_association)
+      reindex_association(record)
+      reindex_associations_process_old_record(reindexable_association)
+    end
+
+    def reindex_association(record)
+      record.reindex_async
+    end
+
+    def reindex_associations_find_old_polymorphic_association(reindexable_association)
+      key, type = reindex_associations_describe_polymorphic_association(reindexable_association)
+      old_klass = reindex_associations_describe_old_polymorphic_association(key, type)
+      return unless old_klass && previous_changes[key] && previous_changes[key].first
+      old_record = old_klass.where(id: previous_changes[key].first).first
+    end
+
+    def reindex_associations_process_old_record(reindexable_association)
+      old_record = reindex_associations_find_old_polymorphic_association(reindexable_association)
+      reindex_association(old_record) if old_record
+    end
+
+    def reindex_associations_describe_polymorphic_association(reindexable_association)
+      key = self.class.reflect_on_association(reindexable_association).try(:foreign_key)
+      type = self.class.reflect_on_association(reindexable_association).try(:foreign_type)
+      [key, type]
+    end
+
+    def reindex_associations_describe_old_polymorphic_association(key, type)
+      if (previous_changes[key] && previous_changes[key].first) || (previous_changes[type] && previous_changes[type].first)
+        old_klass = previous_changes[type].first rescue NameError
+        old_klass = self.send(type).constantize if old_klass == NameError
+      end
+    rescue NameError
+    end
+  end
 end
+
--- test/has_reindexable_associations_test.rb
+++ test/has_reindexable_associations_test.rb
@@ -1,11 +1,318 @@
 require 'test_helper'
 
-class HasReindexableAssociationsTest < Minitest::Test
+class HasReindexableAssociationsTest < ActiveSupport::TestCase
+
+  class HasReindexableAssociation < ActiveRecord::Base
+    include HasReindexableAssociations
+    belongs_to :belongs_to_association
+    has_many :has_many_associations
+    belongs_to :polymorphable, polymorphic: true
+    searchkick
+  end
+
+  class BelongsToAssociation < ActiveRecord::Base
+    include HasReindexableAssociations
+    has_many :has_reindexable_associations
+    searchkick
+  end
+
+  class HasManyAssociation < ActiveRecord::Base
+    include HasReindexableAssociations
+    belongs_to :has_reindexable_association
+    searchkick
+  end
+
+  class BelongsToPolymorphicAssociation < ActiveRecord::Base
+    include HasReindexableAssociations
+    has_many :has_reindexable_associations, as: :polymorphable, dependent: :destroy
+    searchkick
+  end
+
+  setup do
+    connection_info = YAML.load_file("config/database.yml")["test"]
+    ActiveRecord::Base.establish_connection(connection_info)
+
+    ActiveRecord::Schema.define do
+      self.verbose = false
+
+      create_table :belongs_to_associations, :force => true do |t|
+        t.string :text
+      end
+
+      create_table :belongs_to_polymorphic_associations, :force => true do |t|
+        t.string :text
+      end
+
+      create_table :has_reindexable_associations, :force => true do |t|
+        t.string :text
+        t.belongs_to :belongs_to_association, null: false
+        t.references :polymorphable, :polymorphic => true, null: false
+        #low-cardinality index is rails quirk
+        #t.references :polymorphable, :polymorphic => true, null: false, index: {name: 'idx_hra_on_pat_and_pbtpai'}
+      end
+      add_index :has_reindexable_associations, [:polymorphable_id, :polymorphable_type], name: :idx_hra_on_pai_and_pat
+      #low-cardinality index is rails quirk
+      #add_reference :has_reindexable_associations, :belongs_to_polymorphic_association, null: false, index: true, polymorphic: true, name: 'idx_hra_on_pat_andbtpai'
+
+      add_index :has_reindexable_associations, :belongs_to_association_id, :name => 'index_hra_on_btai'
+      add_foreign_key :has_reindexable_associations, :belongs_to_associations, name: :fk_hra_bta_i
+
+      create_table :has_many_associations, :force => true do |t|
+        t.string :text
+        t.belongs_to :has_reindexable_association, null: false
+      end
+
+      add_index :has_many_associations, :has_reindexable_association_id
+      add_foreign_key :has_many_associations, :has_reindexable_associations, name: :fk_hma_hrai_hra_i
+    end
+
+    @belongs_to_association = HasReindexableAssociationsTest::BelongsToAssociation.create
+    @subject = HasReindexableAssociationsTest::HasReindexableAssociation.new
+    @subject.belongs_to_association = @belongs_to_association
+    belongs_to_polymorphable_association = HasReindexableAssociationsTest::BelongsToPolymorphicAssociation.create
+    @subject.polymorphable = belongs_to_polymorphable_association
+    @subject.save
+    HasReindexableAssociationsTest::HasManyAssociation.create(has_reindexable_association_id: @subject.id)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::HasReindexableAssociations::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_has_reindexable_associations
+    HasReindexableAssociationsTest::HasReindexableAssociation.has_reindexable_associations
+    subject = HasReindexableAssociationsTest::HasReindexableAssociation.new
+    args = [:belongs_to_association, :polymorphable, :has_many_associations]
+    subject.class.has_reindexable_associations(*args)
+    assert_equal args, subject.class.class_variable_get(:@@reindexable_associations)
+  end
+
+  def test_reindexable_associations
+    HasReindexableAssociationsTest::HasReindexableAssociation.has_reindexable_associations
+
+    subject = HasReindexableAssociationsTest::HasReindexableAssociation.new
+    assert_instance_of Array, subject.class.reindexable_associations
+    assert_empty subject.class.reindexable_associations
+    subject.class.has_reindexable_associations :belongs_to_association, :polymorphable, :has_many_associations
+    assert_equal [:belongs_to_association, :polymorphable, :has_many_associations], subject.class.reindexable_associations
+  end
+
+  def test_reindex_associations
+    HasReindexableAssociationsTest::HasReindexableAssociation.has_reindexable_associations
+    assert_empty @subject.class.reindexable_associations
+
+    mock = MiniTest::Mock.new
+    returned_value = []
+    arguments_array = []
+    mock.expect(:call, returned_value, arguments_array)
+    @subject.class.stub(:reindexable_associations, mock) do
+      result = @subject.send(:reindex_associations)
+      assert_instance_of Array, result
+      assert_empty result
+    end
+    mock.verify
+
+    mock = MiniTest::Mock.new
+    returned_value = [:belongs_to_association]
+    arguments_array = []
+    mock.expect(:call, returned_value, arguments_array)
+    @subject.class.stub(:reindexable_associations, mock) do
+      assert_not_empty @subject.send(:reindex_associations)
+    end
+    mock.verify
+  end
+
+  def test_reindex_associations_process
+    mock = MiniTest::Mock.new
+    returned_value = [:has_many_associations]
+    arguments_array = [@subject.has_many_associations, returned_value.first]
+    mock.expect(:call, returned_value, arguments_array)
+    @subject.stub(:reindex_associations_process_records, mock) do
+      @subject.send(:reindex_associations_process, returned_value.first)
+    end
+    mock.verify
+
+    @subject.belongs_to_association = @belongs_to_association
+    @subject.save
+
+    mock = MiniTest::Mock.new
+    returned_value = [:belongs_to_association]
+    arguments_array = [@subject.belongs_to_association, returned_value.first]
+    mock.expect(:call, returned_value, arguments_array)
+    @subject.stub(:reindex_associations_process_record, mock) do
+      @subject.send(:reindex_associations_process, returned_value.first)
+    end
+    mock.verify
+  end
+
+  def test_reindex_associations_scope
+    @subject.belongs_to_association = @belongs_to_association
+    @subject.save
+
+    record_or_records  = @subject.belongs_to_association
+
+    assert_raises NoMethodError do
+      @subject.stub(:belongs_to_association, Proc.new { raise NoMethodError }) do
+        result = @subject.send(:reindex_associations_scope, :belongs_to_association)
+      end
+    end
+
+    result = @subject.send(:reindex_associations_scope, :belongs_to_association)
+    assert_equal record_or_records, result
+  end
+
+  def test_reindex_associations_process_records
+    records = @subject.has_many_associations
+    reindexable_association = :has_many_associations
+
+    @subject.class.reindexable_associations_skip = true
+    assert_nil @subject.send(:reindex_associations_process_records, records, reindexable_association)
+
+    @subject.class.reindexable_associations_skip = nil
+
+    mock = MiniTest::Mock.new
+    returned_value = nil
+    arguments_array = [records.first, reindexable_association]
+    mock.expect(:call, returned_value, arguments_array)
+    @subject.stub(:reindex_associations_process_record, mock) do
+      assert_equal [records.first], @subject.send(:reindex_associations_process_records, records, reindexable_association)
+    end
+    mock.verify
+  end
+
+  def test_reindex_associations_process_record
+    records = @subject.has_many_associations
+    reindexable_association = :has_many_associations
+
+    mock = MiniTest::Mock.new
+    returned_value = nil
+    arguments_array = [records.first]
+    mock.expect(:call, returned_value, arguments_array)
+    @subject.stub(:reindex_association, mock) do
+      @subject.send(:reindex_associations_process_record, records.first, reindexable_association)
+    end
+    mock.verify
+
+    mock = MiniTest::Mock.new
+    returned_value = nil
+    arguments_array = [reindexable_association]
+    mock.expect(:call, returned_value, arguments_array)
+    @subject.stub(:reindex_associations_process_old_record, mock) do
+      @subject.send(:reindex_associations_process_record, records.first, reindexable_association)
+    end
+    mock.verify
+  end
+
+  def test_reindex_association
+    record = @subject.belongs_to_association
+    reindexable_association = :belongs_to_association
+
+    mock = MiniTest::Mock.new
+    returned_value = nil
+    arguments_array = []
+    mock.expect(:call, returned_value, arguments_array)
+    record.stub(:reindex_async, mock) do
+      @subject.send(:reindex_association, record)
+    end
+    mock.verify
+  end
+
+  def test_reindex_associations_find_old_polymorphic_association
+    reindexable_association = :belongs_to_association
+
+    @subject.belongs_to_association = nil
+    assert_nil @subject.send(:reindex_associations_find_old_polymorphic_association, reindexable_association)
+
+    new_record = HasReindexableAssociationsTest::BelongsToAssociation.create
+    @subject.belongs_to_association = new_record
+    @subject.save
+    assert_nil @subject.send(:reindex_associations_find_old_polymorphic_association, reindexable_association)
+  end
+
+  def test_reindex_associations_find_old_polymorphic_association_2
+    record = @subject.polymorphable
+    reindexable_association = :polymorphable
+
+    assert_not_empty record.has_reindexable_associations
+
+    new_association = HasReindexableAssociationsTest::BelongsToPolymorphicAssociation.new
+
+    belongs_to_association = HasReindexableAssociationsTest::BelongsToAssociation.create
+    subject = HasReindexableAssociationsTest::HasReindexableAssociation.new
+    subject.belongs_to_association = belongs_to_association
+    belongs_to_polymorphable_association = HasReindexableAssociationsTest::BelongsToPolymorphicAssociation.create
+    subject.polymorphable = belongs_to_polymorphable_association
+    subject.save
+
+    new_association.has_reindexable_associations << subject
+
+    assert_nil subject.send(:reindex_associations_find_old_polymorphic_association, reindexable_association).try(:polymorphable_id)
+
+    new_association.save
+
+    assert_nil subject.send(:reindex_associations_find_old_polymorphic_association, reindexable_association).try(:polymorphable_id)
+
+    belongs_to_polymorphable_association = HasReindexableAssociationsTest::BelongsToPolymorphicAssociation.create
+    subject.polymorphable = belongs_to_polymorphable_association
+    subject.save
+    assert_not_equal new_association.id, subject.send(:reindex_associations_find_old_polymorphic_association, reindexable_association).try(:polymorphable_id)
+  end
+
+  def test_reindex_associations_process_old_record
+    reindexable_association = :belongs_to_association
+
+    new_record = HasReindexableAssociationsTest::BelongsToAssociation.create
+    @subject.belongs_to_association = new_record
+    @subject.save
+
+    record = @subject.belongs_to_association
+    reindexable_association = :belongs_to_association
+
+    mock = MiniTest::Mock.new
+    returned_value = record
+    arguments_array = [reindexable_association]
+    mock.expect(:call, returned_value, arguments_array)
+
+    mock2 = MiniTest::Mock.new
+    returned_value = 'job was enqueued'
+    arguments_array = [record]
+    mock2.expect(:call, returned_value, arguments_array)
+
+    @subject.stub(:reindex_associations_find_old_polymorphic_association, mock) do
+      @subject.stub(:reindex_association, mock2) do
+        assert_equal 'job was enqueued', @subject.send(:reindex_associations_process_old_record, reindexable_association)
+      end
+    end
+    mock.verify
+    mock2.verify
+  end
+
+  def test_reindex_associations_describe_polymorphic_association
+    reindexable_association = :polymorphable
+    key, type = ['polymorphable_id', 'polymorphable_type']
+    assert_equal [key, type], @subject.send(:reindex_associations_describe_polymorphic_association, reindexable_association)
+  end
+
+  def test_reindex_associations_describe_old_polymorphic_association
+    belongs_to_association = HasReindexableAssociationsTest::BelongsToAssociation.create
+    subject = HasReindexableAssociationsTest::HasReindexableAssociation.new
+    subject.belongs_to_association = belongs_to_association
+    belongs_to_polymorphable_association = HasReindexableAssociationsTest::BelongsToPolymorphicAssociation.create
+    subject.polymorphable = belongs_to_polymorphable_association
+    subject.save!
+
+    key, type = ['polymorphable_id', 'polymorphable_type']
+
+    assert_nil subject.send(:reindex_associations_describe_old_polymorphic_association, key, type)
+    assert_nil subject.send(:reindex_associations_describe_old_polymorphic_association, key, :non_existing_type)
+
+    belongs_to_polymorphable_association = HasReindexableAssociationsTest::BelongsToPolymorphicAssociation.create
+    subject.polymorphable = belongs_to_polymorphable_association
+    subject.save!
+
+    belongs_to_polymorphable_association = HasReindexableAssociationsTest::BelongsToPolymorphicAssociation.create
+    subject.polymorphable = belongs_to_polymorphable_association
+
+    assert_equal subject.polymorphable.class, subject.send(:reindex_associations_describe_old_polymorphic_association, key, type)
   end
 end
